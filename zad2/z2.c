c15eacf992243480b71d50617ba0c9c363b07421
Ivan Vrlić <ivrlic02@gmail.com>
2022-10-27 14:56:25 +0200

    Update z2.c

--- zad2/z2.c
+++ zad2/z2.c
@@ -10,7 +10,7 @@ typedef struct _person* position;
 typedef struct _person {
 	char name[MAX_NAME];
 	char lastName[MAX_NAME];
-	int age;
+	int year;
 	position next;
 }Person;
 
@@ -26,7 +26,7 @@ int main(int argc, char** argv)
 	Person head = {
 		.name = { 0 },
 		.lastName = { 0 },
-		.age = { 0 },
+		.year = { 0 },
 		.next = NULL
 	};
 	char ch;
@@ -57,4 +57,58 @@ int main(int argc, char** argv)
 	}
 
 	return 0;
+}
+
+int addFirst(position p) 
+{
+	pozicija q = NULL;
+	q = (pozicija)malloc(sizeof(Person));
+
+	if (q == NULL) {
+		printf("Memorija nije alocirana!\n");
+		return 1;
+	}
+
+	printf("Ime osobe:");
+	scanf("%s", q->name);
+	printf("Prezime osobe:");
+	scanf("%s", q->surname);
+	printf("Godina rodenja osobe:");
+	scanf("%d", &q->year);
+
+	q->next = p->next;
+	p->next = q;
+
+	return 0;
+}
+int searchByLastName(position p) 
+{
+	char pr[MAX_NAME];
+	printf("\nUnesite prezime osobe koju zelite pronaci:");
+	scanf(" %s", &pr);
+	int i = 0;
+
+	while (p != NULL)
+	{
+		if (strcmp(pr, p->surname) == 0) {
+			printf("\nAdresa od osobe %s %s je: %p\n", p->name, p->surname, p);
+			i++;
+		}
+
+		p = p->next;
+
+
+	}
+
+	if (i == 0)
+		printf("Osoba s trazenim prezimenom nije pronadena!\n");
+
+	return 0;
+}
+int toup(char c) 
+{
+	if ('a' <= c <= 'z')
+		c -= ('a' - 'A');
+
+	return c;
 }
